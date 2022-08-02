5547d684b09cb1b465633b0064bdffe83086e1c2
Jack Jenkins <m-gear_sco@hotmail.co.uk>
2022-08-02 08:52:40 +0200

    removing colours within logger

--- lightweaver/logger.py
+++ lightweaver/logger.py
@@ -3,18 +3,26 @@ import logging
 class CustomFormatter(logging.Formatter):
     """Logging Formatter to add colors and count warning / errors"""
 
-    grey = "\x1b[38;21m"
-    yellow = "\x1b[33;21m"
-    red = "\x1b[31;21m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
+    # grey = "\x1b[38;21m"
+    # yellow = "\x1b[33;21m"
+    # red = "\x1b[31;21m"
+    # bold_red = "\x1b[31;1m"
+    # reset = "\x1b[0m"
+
+    # FORMATS = {
+    #     logging.DEBUG: "lw : [" + grey + "%(levelname)-8s" + reset +"]: " + "%(asctime)s:  %(name)s - %(message)s (%(filename)s:%(lineno)d)",
+    #     logging.INFO: "lw : [" + grey + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s",
+    #     logging.WARNING: "lw : [" + yellow + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s",
+    #     logging.ERROR: "lw : [" + red + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s" + " (%(filename)s:%(lineno)d)",
+    #     logging.CRITICAL: "lw : [" + bold_red + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s" + " (%(filename)s:%(lineno)d)"
+    # }
 
     FORMATS = {
-        logging.DEBUG: "lw : [" + grey + "%(levelname)-8s" + reset +"]: " + "%(asctime)s:  %(name)s - %(message)s (%(filename)s:%(lineno)d)",
-        logging.INFO: "lw : [" + grey + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s",
-        logging.WARNING: "lw : [" + yellow + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s",
-        logging.ERROR: "lw : [" + red + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s" + " (%(filename)s:%(lineno)d)",
-        logging.CRITICAL: "lw : [" + bold_red + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s" + " (%(filename)s:%(lineno)d)"
+        logging.DEBUG: "lw : ["+"%(levelname)-8s"+"]: " + "%(asctime)s:  %(name)s - %(message)s (%(filename)s:%(lineno)d)",
+        logging.INFO: "lw : ["+"%(levelname)-8s"+"]: " + "%(asctime)s: " + "%(message)s",
+        logging.WARNING: "lw : ["+"%(levelname)-8s"+"]: " + "%(asctime)s: " + "%(message)s",
+        logging.ERROR: "lw : ["+"%(levelname)-8s"+"]: " + "%(asctime)s: " + "%(message)s" + " (%(filename)s:%(lineno)d)",
+        logging.CRITICAL: "lw : ["+"%(levelname)-8s"+"]: " + "%(asctime)s: " + "%(message)s" + " (%(filename)s:%(lineno)d)"
     }
 
     def format(self, record):
