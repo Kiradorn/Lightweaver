454a0109a4b1c1e50a1aca63afc5a2e49b60c1ac
Jack Jenkins <m-gear_sco@hotmail.co.uk>
2022-08-19 12:38:54 +0200

    add MPI capabilities to logger

--- lightweaver/logger.py
+++ lightweaver/logger.py
@@ -30,13 +30,65 @@ class CustomFormatter(logging.Formatter):
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
-def buildLogger(NameAtCall):
+class MPIFileHandler(logging.FileHandler):                                      
+    def __init__(self,
+                 filename,
+                 mode=MPI.MODE_WRONLY|MPI.MODE_CREATE|MPI.MODE_APPEND ,
+                 encoding='utf-8',  
+                 delay=False,
+                 comm=MPI.COMM_WORLD ):                                                
+        self.baseFilename = abspath(filename)                           
+        self.mode = mode                                                        
+        self.encoding = encoding                                            
+        self.comm = comm                                                        
+        if delay:                                                               
+            #We don't open the stream, but we still need to call the            
+            #Handler constructor to set level, formatter, lock etc.             
+            logging.Handler.__init__(self)                                      
+            self.stream = None                                                  
+        else:                                                                   
+           logging.StreamHandler.__init__(self, self._open())                   
+                                                                                
+    def _open(self):                                                            
+        stream = MPI.File.Open( self.comm, self.baseFilename, self.mode )     
+        stream.Set_atomicity(True)                                              
+        return stream
+                                                    
+    def emit(self, record):
+        """
+        Emit a record.
+        If a formatter is specified, it is used to format the record.
+        The record is then written to the stream with a trailing newline.  If
+        exception information is present, it is formatted using
+        traceback.print_exception and appended to the stream.  If the stream
+        has an 'encoding' attribute, it is used to determine how to do the
+        output to the stream.
+        
+        Modification:
+            stream is MPI.File, so it must use `Write_shared` method rather
+            than `write` method. And `Write_shared` method only accept 
+            bytestring, so `encode` is used. `Write_shared` should be invoked
+            only once in each all of this emit function to keep atomicity.
+        """
+        try:
+            msg = self.format(record)
+            stream = self.stream
+            stream.Write_shared((msg+self.terminator).encode(self.encoding))
+            #self.flush()
+        except Exception:
+            self.handleError(record)        
+
+def buildLogger(NameAtCall, MPI=False):
     logger = logging.getLogger(NameAtCall)
     if not logger.handlers:
 	    logger.setLevel(logging.DEBUG)
-	    ch = logging.StreamHandler()
-	    ch.setLevel(logging.DEBUG)
-	    ch.setFormatter(CustomFormatter())
+	    if MPI:
+            ch = MPIFileHandler("MPI_logfile.log") 
+        else:
+            ch = logging.StreamHandler()
+    	
+        ch.setLevel(logging.DEBUG)    
+        ch.setFormatter(CustomFormatter())
 	    logger.addHandler(ch)
     return logger
 
