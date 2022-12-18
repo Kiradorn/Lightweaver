f3e1119c20e24744a5b6b95bbe8389136d49b49e
Jack Jenkins <m-gear_sco@hotmail.co.uk>
2022-12-18 22:53:28 +0100

    Updated Logger

--- lightweaver/logger.py
+++ lightweaver/logger.py
@@ -1,21 +1,24 @@
 import logging
 
+from mpi4py import MPI
+
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from mpi4py import MPI
+
+    _MPI_APPEND_MODE = MPI.MODE_CREATE | MPI.MODE_APPEND
+
+logging.getLogger(__name__)
+
+
+'''
+Might be nice to add some rank information to the printed statements at some point for the MPI case
+'''
+
 class CustomFormatter(logging.Formatter):
-    """Logging Formatter to add colors and count warning / errors"""
-
-    # grey = "\x1b[38;21m"
-    # yellow = "\x1b[33;21m"
-    # red = "\x1b[31;21m"
-    # bold_red = "\x1b[31;1m"
-    # reset = "\x1b[0m"
-
-    # FORMATS = {
-    #     logging.DEBUG: "lw : [" + grey + "%(levelname)-8s" + reset +"]: " + "%(asctime)s:  %(name)s - %(message)s (%(filename)s:%(lineno)d)",
-    #     logging.INFO: "lw : [" + grey + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s",
-    #     logging.WARNING: "lw : [" + yellow + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s",
-    #     logging.ERROR: "lw : [" + red + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s" + " (%(filename)s:%(lineno)d)",
-    #     logging.CRITICAL: "lw : [" + bold_red + "%(levelname)-8s" + reset +"]: " + "%(asctime)s: " + "%(message)s" + " (%(filename)s:%(lineno)d)"
-    # }
 
     FORMATS = {
         logging.DEBUG: "lw : ["+"%(levelname)-8s"+"]: " + "%(asctime)s:  %(name)s - %(message)s (%(filename)s:%(lineno)d)",
@@ -30,18 +33,130 @@ class CustomFormatter(logging.Formatter):
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
-def buildLogger(NameAtCall):
-    logger = logging.getLogger(NameAtCall)
-    if not logger.handlers:
-	    logger.setLevel(logging.DEBUG)
-	    ch = logging.StreamHandler()
-	    ch.setLevel(logging.DEBUG)
-	    ch.setFormatter(CustomFormatter())
-	    logger.addHandler(ch)
-    return logger
+class MPIFileStream:
+    """Wrap MPI.File` so it has the same API as python file streams.
+
+    Parameters
+    ----------
+    filename : Path
+        disk location of the file stream
+    MPI : MPI
+        MPI communicator object
+    mode : str, optional
+        file write mode, by default _MPI_APPEND_MODE
+    """
+
+    def __init__(
+        self, filename: "Path", MPI: "MPI", mode: str = "_MPI_APPEND_MODE"
+    ) -> None:
+        self.stream = MPI.File.Open(MPI.COMM_WORLD, filename, mode)
+        self.stream.Set_atomicity(True)
+        self.name = "MPIfilestream"
+
+    def write(self, msg: str):
+        """Write to MPI shared file stream.
+
+        Parameters
+        ----------
+        msg : str
+            message to write
+        """
+        b = bytearray()
+        b.extend(map(ord, msg))
+        self.stream.Write_shared(b)
+
+    def close(self):
+        """Synchronize and close MPI file stream."""
+        self.stream.Sync()
+        self.stream.Close()
+
+
+class MPIHandler(logging.FileHandler):
+    """Emulate `logging.FileHandler` with MPI shared File that all ranks can write to.
+
+    Parameters
+    ----------
+    filename : Path
+        file path
+    MPI : MPI
+        MPI communicator object
+    mode : str, optional
+        file access mode, by default "_MPI_APPEND_MODE"
+    """
+
+    def __init__(
+        self,
+        filename: "Path",
+        MPI: "MPI",
+        mode: str = "_MPI_APPEND_MODE",
+    ) -> None:
+        self.MPI = MPI
+        super().__init__(filename, mode=mode, encoding=None, delay=False)
+
+    def _open(self):
+        return MPIFileStream(self.baseFilename, self.MPI, self.mode)
+
+    def setStream(self, stream):
+        """Stream canot be reasigned in MPI mode."""
+        raise NotImplementedError("Unable to do for MPI file handler!")   
+
+
+
+# def buildLogger(NameAtCall, loggerMPI = False):
+def buildLogger(
+    level: int,
+    log_path: Optional["Path"] = None,
+    mpi_log: Optional[bool] = False,
+    root_only: Optional[bool] = True
+):
+
+    
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+
+    MPI = None
+    if mpi_log:
+        try:
+            from mpi4py import MPI
+        except ImportError as e:
+            raise RuntimeError("You cannot specify 'loggerMPI' when mpi4py not installed") from e
+
+    ch = logging.StreamHandler()
+    ch.setFormatter(CustomFormatter())
+    ch.setLevel(level)
+
+    if root_only:
+        if MPI.COMM_WORLD.Get_rank() == 0.:
+            logger.addHandler(ch)
+    else:
+        logger.addHandler(ch)
+
+    if log_path:
+        
+        fh = None
+
+        if mpi_log:
+            fh = MPIHandler(log_path, MPI, mode=MPI.MODE_WRONLY | MPI.MODE_CREATE | MPI.MODE_APPEND)
+            fh.setFormatter(CustomFormatter())
+        else:
+            fh = logging.FileHandler(log_path, mode=MPI.MODE_WRONLY | MPI.MODE_CREATE | MPI.MODE_APPEND)
+            fh.setFormatter(CustomFormatter())
+
+        if fh:
+            if root_only:
+                if MPI.COMM_WORLD.Get_rank() == 0.:
+                    fh.setLevel(level)
+                    logger.addHandler(fh)
+            else:
+                fh.setLevel(level)
+                logger.addHandler(fh)
+            
+
 
 def closeLogger(logger):
-	handlers = logger.handlers[:]
-	for handler in handlers:
-	    logger.removeHandler(handler)
-	    handler.close()
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+        handler.close()
