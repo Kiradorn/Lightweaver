d3e0c5bbc1ebfaeb2286f8a571ade004e15050b9
Jack Jenkins <m-gear_sco@hotmail.co.uk>
2023-03-13 19:19:10 +0100

    format prints

--- Source/LwMiddleLayer.pyx
+++ Source/LwMiddleLayer.pyx
@@ -783,7 +783,7 @@ cdef class LwAtmosphere:
             verify_bc_array_sizes(&self.atmos.zLowerBc, bc, 'zLowerBc')
             data = f64_view_3(bc)
             self.atmos.zLowerBc.set_bc_data(data)
-            log.info("self.pyAtmos.zLowerBc.compute_bc(self.pyAtmos, spect)[500,0,30] in MpiStackedContext: " + str(bc[500,0,30]))
+            log.info("\nself.pyAtmos.zLowerBc.compute_bc(self.pyAtmos, spect)[500,0,30] in LwMiddleLayer: " + str(bc[500,0,30]))
             # log.info(&self.atmos.zLowerBc.bcData)
 
         if self.atmos.zUpperBc.type == CALLABLE:
